d8ea11a12d6a48860617ded3b5e9b0d17f35bcb6
shareefshareef <mahaboobshareef831@gmail.com>
2024-06-03 16:04:43 +0530

    pg2

--- propic/models.py
+++ propic/models.py
@@ -7,3 +7,5 @@ from django.contrib.auth.models import User
 class AccountImages(models.Model):
     iuser = models.ForeignKey(User,on_delete=models.CASCADE)
     iimage = models.ImageField(upload_to="folderpropics")
+    description = models.CharField(max_length=500)
+    pub_date = models.DateTimeField(auto_now_add=True)
--- propic/views.py
+++ propic/views.py
@@ -6,13 +6,14 @@ from .models import AccountImages
 def get_image(request):
     user = request.user
     if request.method == "POST":
+        description = request.POST.get("description")
         image = request.FILES.get("image")
-        obj = AccountImages.objects.create(iuser=user,iimage = image)
+        obj = AccountImages.objects.create(iuser=user,iimage = image,description=description)
         return redirect("/get-image")
 
     return render(request,"add_image.html")
 
 def getallpics(request):
-    obj = AccountImages.objects.all()
+    obj = AccountImages.objects.all().order_by("-id")
     
     return render(request,"allpics.html",context={"data":obj})
